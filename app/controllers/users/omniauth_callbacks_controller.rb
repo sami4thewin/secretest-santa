1cb11db4eb51e970c200491e2fca05018cd494db
sami4thewin <samiomarmohammed@gmail.com>
2016-12-07 01:19:06 -0500

    Added final touches for Oauth and Devise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user.name = request.env["omniauth.auth"]['info']['name']
+    # binding.pry
     sign_in_and_redirect @user
   end
 end
