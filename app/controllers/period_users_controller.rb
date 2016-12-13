bc4a3dbba09a39be746b1fae35929a2fffe6fd46
sami4thewin <samiomarmohammed@gmail.com>
2016-12-13 01:21:42 -0500

    Added users to periods

--- app/controllers/period_users_controller.rb
+++ app/controllers/period_users_controller.rb
@@ -1,5 +1,12 @@
 class PeriodUsersController < ApplicationController
 
-  
+  protect_from_forgery
+
+  def create
+    # binding.pry
+    @user = User.find(params[:user_id])
+    @period = Period.find(params[:period_id])
+    @period.users << current_user
+  end
 
 end
